093691858e11510cc3e4f3925202aa23a68b7188
Alex Williams <alexjwilliams1@gmail.com>
2021-04-03 11:37:06 -0700

    mostly style updates

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
-import {coding} from "./data"
+import { coding } from "./data"
 
 import MemoryCard from "./MemoryCard"
 
-
 function App() {
-  
+
   const [memCards, setMemCards] = useState([])
   const [collection, setCollection] = useState(coding)
-  const [selection, setSelection] = useState([])
-
+  const [selection, setSelection] = useState()
+  const [matches, setMatches] = useState([])
+  const [guesses, setGuesses] = useState(0)
 
   useEffect(() => {
     setMemCards(duplicate(collection))
-  },[collection])
+  }, [collection])
+
+
 
   const duplicate = (cards) => {
     let dupCards = []
-    for (let i = 0; i < 2 * (cards.length); i++){
-      let newCard = {...cards[i % cards.length], key: cards[i % cards.length].titl+i}
+    for (let i = 0; i < 2 * (cards.length); i++) {
+      let newCard = { ...cards[i % cards.length], key: cards[i % cards.length].title + i, match: false, selected: false }
       dupCards.push(newCard)
     }
     return dupCards
   }
 
+  const checkMatch = (firstCard, secondCard) => {
+    if (firstCard.title === secondCard.title) {
+      setMatches([...matches, firstCard, secondCard])
+      firstCard.match = true
+      secondCard.match = true
+    }
+  }
+
+  console.log(memCards)
   return (
     <div className="App">
       <header>
-        <button>Randomize</button>
+        <h1 className='logo'>PokeMatch</h1>
+        {/* <button>Randomize</button> */}
+        <div className="score-board-area">
+          <div className="score-board">
+            <h2>Matches</h2>
+            <p>0 / 8</p>
+          </div>
+          <div className="score-board">
+            <h2>Guesses</h2>
+            <p>{guesses}</p>
+          </div>
+          <div className="score-board">
+            <h2>Time</h2>
+            <p>0/8</p>
+          </div>
+        </div>
       </header>
       <div className="card-area">
 
         {memCards.map(cardData => {
-          return <MemoryCard key={cardData.key} card={cardData}/>
+          return <MemoryCard key={cardData.key} card={cardData} setSelection={setSelection} selection={selection} checkMatch={checkMatch} />
         })}
       </div>
     </div>
--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -1,15 +1,31 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-
+const url = "https://pokeres.bastionbot.org/images/pokemon";
 
 const MemoryCard = (props) => {
 
-    return(
-        <div className="memory-card">
-            <div className="memory-card-inner"></div>
-            <div className='card-title'><h2>{props.card.title}</h2></div>
-            <img src={props.card.src} alt={props.card.alt}/>
+    let flippedOver = false
 
+    const selectCard = () => {
+        if (!props.selection) {
+            props.card.selected = true
+            props.setSelection([props.card])
+        } else if (props.selection && props.card !== props.selection) {
+            props.card.selected = true
+            props.checkMatch(props.selection, props.card)
+        }
+    }
+
+    return (
+        <div className={props.card.selected || props.card.match ? "memory-card selected" : "memory-card"} onClick={props.card.match ? () => { } : selectCard}>
+            <div className="memory-card-inner">
+                <div className='mem-card-back'>
+                </div>
+                <div className="mem-card-face">
+                    <div className='card-title'><h2>{props.card.title}</h2></div>
+                    <img src={`${url}/${280}.png`} alt={props.card.alt} />
+                </div>
+            </div>
         </div>
     )
 }
