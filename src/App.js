5b731a4ffc8c4db1a5567a5259d9730d95e84692
Alex Williams <alexjwilliams1@gmail.com>
2021-04-05 13:53:04 -0700

    initial commit onto main branch, app mostly build

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import './App.css';
+import { dummyPokeData } from "./data"
+
+import MemoryCard from "./components/MemoryCard"
+
+
+function App() {
+
+  const [memCards, setMemCards] = useState([])
+  const [selection, setSelection] = useState()
+  const [matches, setMatches] = useState([])
+  const [guesses, setGuesses] = useState(0)
+  const [phase, setPhase] = useState('start')
+
+
+  useEffect(() => {
+    setMemCards(duplicate(dummyPokeData))
+  }, [])
+
+  const duplicate = (cards) => {
+    let dupCards = []
+    for (let i = 0; i < 2 * (cards.length); i++) {
+      let newCard = { ...cards[i % cards.length], key: cards[i % cards.length].title + i, match: false, selected: false }
+      dupCards.push(newCard)
+    }
+    return dupCards
+  }
+
+  const checkMatch = (firstCard, secondCard) => {
+    if (firstCard.title === secondCard.title) {
+      setMatches([...matches, firstCard, secondCard])
+      firstCard.match = true
+      secondCard.match = true
+    }
+  }
+
+
+  return (
+    <div className="App">
+      <header>
+        <h1 className='logo'>PokeMatch</h1>
+        <div className="score-board-area">
+          <div className="score-board">
+            <h2>Matches</h2>
+            <p>{matches.length / 2} / 8</p>
+          </div>
+          <div className="score-board">
+            <h2>Guesses</h2>
+            <p>{guesses}</p>
+          </div>
+          <div className="score-board">
+            <h2>Time</h2>
+            <p>0</p>
+          </div>
+        </div>
+      </header>
+      <div className="card-area">
+
+        {memCards.map(cardData => {
+          return <MemoryCard key={cardData.key} card={cardData} setSelection={setSelection} selection={selection} checkMatch={checkMatch} />
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,15 @@
+import pokeball from "./images/pokeball.png"
+
+
+const angular ={src: pokeball, alt:"angular logo", title: "Angular"}
+const css = {src: pokeball, alt:"css logo", title: "CSS"}
+const html = {src: pokeball, alt: "html logo", title: "HTML"}
+const java = {src: pokeball, alt: "java logo", title: "Java"}
+const javaScript = {src: pokeball, alt: "javaScript logo", title: "JavaScript"}
+const postgreSQL = {src: pokeball, alt: "postgres logo", title: "PostgreSQL"}
+const react = {src: pokeball, alt: "react logo", title: "React"}
+const vue = {src: pokeball, alt: "vue logo", title: "Vue"}
+
+export const dummyPokeData = [angular, css, html, java, javaScript, postgreSQL, react, vue]
+
+
